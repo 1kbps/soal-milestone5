b19432f43c32a621be8d4862bd6369ec39ff5f55
School of Accelerated Learning <soal@Schools-Mac-mini-2.local>
2018-02-07 18:39:52 +0530

    algo

---
+++ auction/keepBids.js
@@ -0,0 +1,25 @@
+var pKeepsBids = function (){
+    for (let o of pKeepsBidSequence()){
+    //runs the wicket keeper bid sequence function 
+    //returns array with 6 values
+    //124, 160, 110, 135, 123, 192
+    //take the value at the 0 index & store in new array at index 0
+    //124
+    //pKeepSelect = [174, 110, 124]
+    //loop function 12 times
+    //array 12 values long representing twelve auction prices of wicket keepers
+
+    }
+}
+
+//run bid sequence function
+//return array with 6 values
+//[1,1,3,4,3,5,6,2,4,5,]
+//take the value at zero index and store in new array at index 0 checking for greater than 2 occurences of value 
+//loop function 12 times
+//array 12 values long representing 6 teams that won 2 rounds of bidding
+
+//map array 1 to array 2 
+
+
+
