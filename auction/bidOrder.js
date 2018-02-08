3455517a341d3f9c9a030282b512b594499ca0a4
School of Accelerated Learning <soal@Schools-Mac-mini-2.local>
2018-02-08 13:01:46 +0530

    new randomizer

--- auction/bidOrder.js
+++ auction/bidOrder.js
@@ -2,7 +2,10 @@
 // Durstenfeld Shuffle algorithm for random shuffles of an array
 //retrieved from: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 
-var teamNumbers = [1,2,3,4,5,6];
+var teamString = '1,2,3,4,5,6,'.repeat(4);
+console.log(teamString);
+var teamNumbers = teamString.split(',').map(Number);
+console.log(teamNumbers);
 function shuffleOrder(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
@@ -12,6 +15,7 @@ function shuffleOrder(array) {
     }
 }
 shuffleOrder (teamNumbers);
+console.log (teamNumbers);
 
 // one sequence to generate the bid order for each round of bidding
 
